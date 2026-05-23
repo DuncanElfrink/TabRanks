752173b2c3b8605252204b2c0056b4c0d6ee1336
Duncan Elfrink <59018415+DuncanElfrink@users.noreply.github.com>
2026-05-23 12:28:24 +0200

    Update PlayerListener.java

--- src/main/java/nl/tabranks/plugin/PlayerListener.java
+++ src/main/java/nl/tabranks/plugin/PlayerListener.java
@@ -3,6 +3,7 @@ package nl.tabranks.plugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -10,24 +11,31 @@ public class PlayerListener implements Listener {
 
     private final TabRanks plugin;
     private final TabListManager tabListManager;
+    private final RankManager rankManager;
 
     public PlayerListener(TabRanks plugin, TabListManager tabListManager) {
         this.plugin = plugin;
         this.tabListManager = tabListManager;
+        this.rankManager = plugin.getRankManager();
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onJoin(PlayerJoinEvent event) {
-        // Klein vertraagje zodat de speler volledig is ingelogd
         plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
-            tabListManager.updateAll(); // Herlaad voor alle spelers (nieuwe speler erbij)
+            tabListManager.updateAll();
         }, 5L);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onChat(AsyncPlayerChatEvent event) {
+        Rank rank = rankManager.getRank(event.getPlayer());
+        String format = rank.getPrefix() + "%s" + "§r: %s";
+        event.setFormat(format);
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onQuit(PlayerQuitEvent event) {
         tabListManager.removePlayer(event.getPlayer());
-        // Kleine vertraging zodat de speler echt weg is
         plugin.getServer().getScheduler().runTaskLater(plugin, tabListManager::updateAll, 2L);
     }
 }
